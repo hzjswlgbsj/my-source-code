8c6d886fa968a55e4e1b2a5d7606efb505a1580b
Sixty <hiliulin@aliyun.com>
2021-01-17 14:23:39 +0800

    哈希表

--- 数据结构与算法JavaScript描述/src/index.ts
+++ 数据结构与算法JavaScript描述/src/index.ts
@@ -3,5 +3,6 @@
 // import './chapter4/test-stack';
 // import './chapter5/test-queue';
 // import './chapter6/test-linkedlist';
-import './chapter7/test-dictionary';
+// import './chapter7/test-dictionary';
+import './chapter8/test-hashtable';
 document.title = '测试专用'
