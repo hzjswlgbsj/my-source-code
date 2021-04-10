327e78e3118fcfec77e96aa8c37ad8f97a694ce2
Sixty <hiliulin@aliyun.com>
2021-04-10 18:26:00 +0800

    查找二叉树增加获取最大最小值以及查找方法

--- 数据结构与算法JavaScript描述/src/chapter10/BST.ts
+++ 数据结构与算法JavaScript描述/src/chapter10/BST.ts
@@ -2,15 +2,22 @@ import Node from './Node';
 interface IBST {
   root: any;
   insert: (data: any) => void; // 向二叉查找树中插入值
-  inOrder: (node: Node) => void; // 中序遍历
-  preOrder: (node: Node) => void; // 先序遍历
-  postOrder: (node: Node) => void; // 后序遍历
+  inOrder: (node: Node, cb?: (node: Node) => void) => void; // 中序遍历
+  preOrder: (node: Node, cb?: (node: Node) => void) => void; // 先序遍历
+  postOrder: (node: Node, cb?: (node: Node) => void) => void; // 后序遍历
+  showTree: () => void;
+  getMin: () => any;
+  getMax: () => any;
+  find: (value: any) => Node | null;
 }
 
 // 二叉查找树
 export class BST implements IBST{
   public root: any = null;
 
+  public showTree() {
+    console.log('当前树结构', this.root)
+  }
   public insert(data: any) {
     const node = new Node(data, null, null);
     if (this.root == null) {
@@ -36,25 +43,80 @@ export class BST implements IBST{
       }
     }
   }
-  public inOrder(node: Node) {
+  public inOrder(node: Node, cb?: (node: Node) => void) {
     if (!(node === null)) {
-      this.inOrder(node.left);       
-      node.show();  
-      this.inOrder(node.right);    
+      this.inOrder(node.left, cb);       
+      // node.show();
+      if (cb && typeof cb === 'function') {
+        cb(node);
+      }
+      this.inOrder(node.right, cb);    
     } 
   }
-  public preOrder(node: Node) {
+  public preOrder(node: Node, cb?: (node: Node) => void) {
     if (!(node === null)) {    
-      node.show()
-      this.preOrder(node.left);       
-      this.preOrder(node.right);    
+      // node.show()
+      if (cb && typeof cb === 'function') {
+        cb(node);
+      }
+      this.preOrder(node.left, cb);       
+      this.preOrder(node.right, cb);    
     } 
   }
-  public postOrder(node: Node) {
+  public postOrder(node: Node, cb?: (node: Node) => void) {
     if (!(node === null)) {    
-      this.postOrder(node.left);       
-      this.postOrder(node.right);
-      node.show()
+      this.postOrder(node.left, cb);       
+      this.postOrder(node.right, cb);
+      // node.show()
+      if (cb && typeof cb === 'function') {
+        cb(node);
+      }
     } 
+  }
+
+  public getMin() {
+    let current = this.root;
+    while (current.left !== null) {
+      current = current.left
+    }
+
+    return current.data;
+  }
+  public getMax() {
+    let current = this.root;
+    while (current.right !== null) {
+      current = current.right
+    }
+
+    return current.data;
+  }
+
+  // 使用已经实现的 中序、先序、后序遍历会多执行遍历次数，
+  // 无论什么时候找到对应节点都会将整棵树遍历一遍
+  // public find(data: any) {
+  //   let findNode = null;
+  //   this.inOrder(this.root, (node: Node) => {
+  //     console.log(11111111)
+  //     if (data === node.data) {
+  //       findNode = node
+  //     }
+  //   })
+
+  //   return findNode;
+  // }
+
+  // 这种方式可自己控制跳出遍历的时机
+  public find(data: any) {
+    var current = this.root;
+    while (current != null) {
+      if (current.data == data) {
+        return current;         
+      } else if (data < current.data) {
+        current = current.left;
+      } else {
+        current = current.right;
+      }
+    }
+    return null; 
   }
 }
